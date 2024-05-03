20edb2d82efc6d89dd7ea8b921a3789321ab54f6
Yo Sakaguchi <yo.sakaguchi@sansan.com>
2024-05-03 18:19:10 +0900

    apply fmt

--- src/main.rs
+++ src/main.rs
@@ -334,7 +334,8 @@ mod tests {
         let mut parser = TsParser::new();
         parser.set_language(language()).unwrap();
 
-        let sql = fs::read_to_string("./sql/current_date_and_subquery_with_between_are_used.sql").unwrap();
+        let sql = fs::read_to_string("./sql/current_date_and_subquery_with_between_are_used.sql")
+            .unwrap();
         let tree = parser.parse(&sql, None).unwrap();
 
         let mut diagnostics = Vec::new();
